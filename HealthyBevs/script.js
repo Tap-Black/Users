4e3b82ac97d8642e64814872efd2baecc9203901
Tap-Black <147441075+Tap-Black@users.noreply.github.com>
2023-12-22 04:41:29 +0000

    commit plz

--- HealthyBevs/script.js
+++ HealthyBevs/script.js
@@ -3,6 +3,14 @@ var Titles = ["Golden Goodness",
 "Orange Oasis Cleanse",
 "Pine-Apple"
 ]
+
+var Texts = [
+    "A tropical blend of pineapple, mango, and lemon, offering a refreshing burst of immune-boosting vitamin C, antioxidants, and hydration.",
+    "It's green juice bro. Drink at your own risk",
+    "A healthful juice blend of oranges, carrots, and ginger, offering immune support, antioxidant benefits, and overall well-being",
+    "It's in the name... Let's be so for real"
+
+]
 var currIndex = 0;
 var maxIndex = Titles.length - 1
 
@@ -18,6 +26,9 @@ $(function(){
         }
         $('#title').fadeOut(500,function(){
             $(this).text(Titles[currIndex]).fadeIn(500);
+        });
+        $('#text').fadeOut(500,function(){
+            $(this).text(Texts[currIndex]).fadeIn(500);
         });
 
 
