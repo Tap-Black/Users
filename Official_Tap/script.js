16d18d3cf98737797ad5cb51ba7c621d9edd315e
Tap-Black <147441075+Tap-Black@users.noreply.github.com>
2024-01-30 06:37:56 +0000

    updateeeee

--- Official_Tap/script.js
+++ Official_Tap/script.js
@@ -1,14 +1,106 @@
- document.addEventListener('DOMContentLoaded', function() {
-            // Get the video element
-            var video = document.getElementById('video');
+ var header = document.getElementById("Top");
+ var text = document.getElementById("Bottom");
+ var D1 = document.getElementById("D1");
+ var D2 = document.getElementById("D2");
+ var D3 = document.getElementById("D3");
+ var D4 = document.getElementById("D4");
 
-            // Check if the video element is found
-            if (video && typeof video.play === 'function') {
-                // Delay the video playback by 3000 milliseconds (3 seconds)
-                setTimeout(function() {
-                    video.play();
-                }, 3000);
-            } else {
-                console.error('Video element not found or play method not supported.');
-            }
-        });
+
+var Headers = [
+    "One Card",
+    "Tap",
+    "Connect",
+    "Go"
+]
+
+var Texts = [
+    "ahfadskjflajfklajfsa",
+    "adsjfilkajfskaljfaljf",
+    "Conadsfjlasdkjnect",
+    "adsfafas"
+]
+
+var currIndex = 0;
+
+
+ 
+
+ document.addEventListener('DOMContentLoaded', function() {
+    
+    header.textContent= Headers[currIndex];
+    text.textContent= Texts[currIndex];
+
+        /* Iteration 1*/
+        setTimeout(function() {
+            header.animate([
+                {opacity: "100"},
+                {opacity: "0"},
+                {opacity: "100"}
+
+
+            ],{
+                duration: 1000,
+                fill:'forwards'
+            });
+            text.animate([
+                {opacity: "100"},
+                {opacity: "0"},
+                {opacity: "100"}
+
+
+            ],{
+                duration: 1000,
+                fill:'forwards'
+            });
+
+            D1.style.backgroundColor = "#1F1f1f";
+            D2.style.backgroundColor = "#C2E812";
+
+          }, 7000);
+           
+      
+        setTimeout(function(){
+            currIndex++;
+            header.textContent = Headers[currIndex];
+            text.textContent = Texts[currIndex];
+        },7500);
+        
+        
+        /* Iteration 1*/
+        setTimeout(function() {
+            header.animate([
+                {opacity: "100"},
+                {opacity: "0"},
+                {opacity: "100"}
+
+
+            ],{
+                duration: 1000,
+                fill:'forwards'
+            });
+            text.animate([
+                {opacity: "100"},
+                {opacity: "0"},
+                {opacity: "100"}
+
+
+            ],{
+                duration: 1000,
+                fill:'forwards'
+            });
+
+            D2.style.backgroundColor = "#1F1f1f";
+            D3.style.backgroundColor = "#C2E812";
+
+          }, 10000);
+           
+      
+        setTimeout(function(){
+            currIndex++;
+            header.textContent = Headers[currIndex];
+            text.textContent = Texts[currIndex];
+        },10500);
+    
+        
+
+});
